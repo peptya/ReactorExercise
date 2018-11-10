45012706fd1465027d2337b252f94f6c59d21e32
bridgeit <pranitkokne2018@gmail.com>
2018-11-10 17:44:57 +0530

    :)

---
+++ reactor/src/main/java/com/bridgelabz/reactor/flux/DoOnDiscardOperation.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.reactor.flux;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import reactor.core.publisher.Flux;
+
+public class DoOnDiscardOperation {
+
+	public static void main(String[] args) {
+
+		AtomicInteger discardedCount = new AtomicInteger();
+
+		Flux.range(1, 20).filter(i -> i % 2 == 0).doOnDiscard(Number.class, i -> discardedCount.getAndIncrement())
+				.subscribe(e -> System.out.println(e));
+
+		System.out.println(discardedCount);
+	}
+
+}
